f88f5db5e864b636e153d4900ed1d96a4bd6be4b
Dennizz <root@ipaddr.nl>
2024-11-14 12:17:15 +0100

    fix var and bump copilot init module version

--- main.tf
+++ main.tf
@@ -4,7 +4,7 @@ module "controller_build" {
 
 
   // please do not use special characters such as `\/"[]:|<>+=;,?*@&~!#$%^()_{}'` in the controller_name
-  controller_name      = "test"
+  controller_name      = var.controller_name
   incoming_ssl_cidrs   = var.incoming_ssl_cidrs
   use_existing_network = var.use_existing_network
   access_account_name  = "GCP"
@@ -67,7 +67,7 @@ module "copilot_init" {
   count = var.module_config.copilot_initialization ? 1 : 0
 
   source  = "terraform-aviatrix-modules/copilot-init/aviatrix"
-  version = "v1.0.2"
+  version = "v1.0.3"
 
   controller_public_ip             = module.controller_build[0].controller_public_ip_address
   controller_admin_password        = var.controller_admin_password
